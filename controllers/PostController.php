27c6832d6b2852ebaccebcaacd221fa5ad96d1a3
Sentinel Corps <deepspacesentinelcorps@gmail.com>
2023-06-08 13:29:19 +0200

    Goedgekeurd

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -6,16 +6,23 @@ class PostController extends BaseController
 {
     public function create()
     {
+        $error = $_GET['error'] ?? null;
         $this->authorizeUser($_SESSION['userid']);
 
         $profileImg = $this->getProfileImg();
 
         $user = R::find('users', 'id LIKE :id', [':id' => $_SESSION['userid']]);
-        displayTwig('/post/create.twig', ['user' => $user, 'profileImg' => $profileImg]);
+        displayTwig('/post/create.twig', ['error' => $error, 'user' => $user, 'profileImg' => $profileImg]);
     }
 
     public function createPost()
     {
+
+        if (empty(trim($_POST['code-input']))) {
+            header('location: ?error=emptyinput');
+            exit();
+        }
+
         $postBean = R::dispense('posts');
         $postBean->likes = 0;
         $postBean->description = htmlspecialchars(trim($_POST['description']));
